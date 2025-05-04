06fac549faa1c2302cae452c29ab4d25c16a394f
fernando <fernando@juandemariana.org>
2025-05-04 23:56:48 +0200

    Add index page.

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Bienvenido a EnergyAppMockup</h1>
+    <p>Esta es la página de inicio.</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+</script>
