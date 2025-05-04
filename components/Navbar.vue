9cb1e003ac05d7735306b303d69ed0584e6c0261
fernando <fernando@juandemariana.org>
2025-05-04 23:50:53 +0200

    Add default layout

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useTheme } from "~/composables/useTheme";
+const { theme, toggleTheme } = useTheme();
+</script>
+
+<template>
+  <nav>
+    <button @click="toggleTheme">
+      Cambiar a {{ theme === "light" ? "oscuro" : "claro" }}
+    </button>
+  </nav>
+</template>
