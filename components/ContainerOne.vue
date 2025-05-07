73e5d51d60156e43d9bdbff114eb75a963746126
fernando <fernando@juandemariana.org>
2025-05-07 23:59:20 +0200

    Add ContainerOne

---
+++ components/ContainerOne.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="contenedor">
+    <component :is="componente" v-bind="props"></component>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  componente: {
+    type: [Object, String],
+    required: true
+  },
+  props: {
+    type: Object,
+    default: () => ({})
+  }
+});
+</script>
+
+<style scoped>
+.contenedor {
+  width: 90vw;
+  margin: auto;
+  border: 2px solid #000;
+  padding: 20px;
+}
+</style>
--- components/DividingLine.vue
+++ components/DividingLine.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="loader">
-    <div class="light"></div>
     <div class="black_overlay"></div>
+    <div class="light"></div>
+
   </div>
 </template>
 
@@ -18,16 +19,17 @@
 .loader .black_overlay {
   background: linear-gradient(
     87deg,
-    rgb(var(--text-color-rgb), 0.14) 0%,
+    rgb(var(--text-color-rgb), 0.07) 0%,
     rgba(var(--text-color-rgb)) 30%,
     rgba(var(--text-color-rgb)) 70%,
-    rgb(var(--text-color-rgb), 0.14) 100%
+    rgb(var(--text-color-rgb), 0.07) 100%
   );
   position: absolute;
   inset: 0px;
 }
 
 .loader .light {
+  --yellow: #e3f86c;
   width: 70px;
   height: 100%;
   position: absolute;
@@ -35,20 +37,40 @@
   top: 0px;
   background: linear-gradient(
     87deg,
-    var(--income-color) 0%,
+    var(--yellow) 0%,
     var(--secondary-color) 40%,
     var(--secondary-color) 60%,
-    var(--income-color) 100%
+    var(--yellow) 100%
   );
   animation: light 2s infinite ease-in-out;
 }
 
-@keyframes light {
-  from {
-    left: -30%;
-  }
-  to {
-    left: 100%;
+.light {
+  will-change: transform;
+}
+
+
+@media (prefers-reduced-motion: reduce) {
+  .light {
+    animation: none;
   }
 }
+
+
+@keyframes light {
+  0% { left: -100%; opacity: 0.1; }
+  10% { left: 0%; opacity: 0.4; }
+  90% { left: 100%; opacity: 0.4; }
+  100% { left: 100%; opacity: 0.1; }
+}
+
+
+
+.light {
+  animation: light 4s infinite cubic-bezier(0.65, 0, 0.35, 1);
+}
+
+
+
+
 </style>
