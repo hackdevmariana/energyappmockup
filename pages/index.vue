3b34f42dc255f386138cf6863e9c29c5a8674b7d
fernando <fernando@juandemariana.org>
2025-05-11 23:51:21 +0200

    Add container-center class.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <h1>Colecciones de componentes</h1>
+  <div class="container-center">
+    <h1>Colecciones de componentes</h1>
 
-  <HoverCard colorClass="red" tipText="Gráficos" link="/components/charts" />
-
-  <HoverCard
-    colorClass="green"
-    tipText="Contenedores"
-    link="/components/containers"
-  />
+    <HoverCard colorClass="red" tipText="Gráficos" link="/components/charts" />
+    <HoverCard
+      colorClass="green"
+      tipText="Contenedores"
+      link="/components/containers"
+    />
+  </div>
 </template>
+
+<style scoped>
+.container-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+  text-align: center;
+}
+</style>
