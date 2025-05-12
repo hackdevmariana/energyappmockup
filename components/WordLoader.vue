9c4c3318e83c378dfe8019cac404910f585484fc
fernando <fernando@juandemariana.org>
2025-05-12 09:54:36 +0200

    Add WordLoader component

--- components/WordLoader.vue
+++ components/WordLoader.vue
@@ -3,9 +3,14 @@
     <div class="loader">
       <p>loading</p>
       <div class="words">
-        <span v-for="(word, index) in words" :key="index" class="word">{{
-          word
-        }}</span>
+        <span
+          v-for="(word, index) in words"
+          :key="index"
+          class="word"
+          :style="{ animationDelay: `${index * 0.5}s` }"
+        >
+          {{ word }}
+        </span>
       </div>
     </div>
   </div>
@@ -23,24 +28,28 @@ defineProps({
 
 <style scoped>
 .card {
-  --bg-color: #212121;
+  --bg-color: var(--background-color);
   background-color: var(--bg-color);
   padding: 1rem 2rem;
   border-radius: 1.25rem;
 }
+
 .loader {
-  color: rgb(124, 124, 124);
-  font-family: "Poppins", sans-serif;
+  color: var(--text-color);
+  font-family: var(--font-featured);
   font-weight: 500;
   font-size: 25px;
   display: flex;
   align-items: center;
   border-radius: 8px;
 }
+
 .words {
   overflow: hidden;
   position: relative;
+  font-family: var(--font-general);
 }
+
 .words::after {
   content: "";
   position: absolute;
@@ -53,36 +62,44 @@ defineProps({
   );
   z-index: 20;
 }
+
 .word {
   display: block;
   padding-left: 6px;
-  color: #956afa;
+  color: var(--primary-color);
+  opacity: 0;
   animation: spin_4991 4s infinite;
+  animation-timing-function: ease-in-out;
 }
+
 @keyframes spin_4991 {
-  10% {
-    transform: translateY(-102%);
+  0%,
+  100% {
+    transform: translateY(0);
+    opacity: 1;
   }
+
+  10%,
   25% {
-    transform: translateY(-100%);
+    transform: translateY(-102%);
+    opacity: 1;
   }
-  35% {
-    transform: translateY(-202%);
-  }
+
+  35%,
   50% {
     transform: translateY(-200%);
+    opacity: 1;
   }
-  60% {
-    transform: translateY(-302%);
-  }
+
+  60%,
   75% {
     transform: translateY(-300%);
+    opacity: 1;
   }
+
   85% {
-    transform: translateY(-402%);
-  }
-  100% {
     transform: translateY(-400%);
+    opacity: 0;
   }
 }
 </style>
