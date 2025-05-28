0c39065ff4ed1516c405d940a90b5951dc617455
Fernando <fernando@juandemariana.org>
2025-05-28 21:54:02 +0200

    Add pages/todaysprice.

--- pages/components/pages.vue
+++ pages/components/pages.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import Todaysprice from '../pages/todaysprice.vue';
+
+</script>
+
 <template>
   <div class="container-center">
-    <h1>Precios de hoy</h1>
-    <ElectricityPriceToday />
+    <ButtonComponent :text="'Precios de hoy'" :url="'~/pages/todaysprice'" />
+
     <h1>Precios de mañana</h1>
     <ElectricityPriceTomorrow />
     <h1>Seleccionar el día</h1>
