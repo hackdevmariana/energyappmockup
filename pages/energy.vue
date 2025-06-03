a2227ff8198821a8e3bdac12f8b2ee842b119fda
Fernando <fernando@juandemariana.org>
2025-06-03 23:59:34 +0200

    Add ConsumedEnergy component.

--- pages/energy.vue
+++ pages/energy.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import GeneratedEnergy from '~/components/energy/GeneratedEnergy.vue';
+import ConsumedEnergy from '~/components/energy/ConsumedEnergy.vue';
 
 </script>
 
 <template>
     <GeneratedEnergy :energy="8.2"/>
+    <ConsumedEnergy :energy="6.1"/>
 </template>
