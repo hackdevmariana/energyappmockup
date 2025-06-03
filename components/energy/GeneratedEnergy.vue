28a04355d2530cf6e1da243cbcdfcab51aa0de7d
Fernando <fernando@juandemariana.org>
2025-06-03 23:22:11 +0200

    Add GeneratedEnergy component.

---
+++ components/energy/GeneratedEnergy.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="energy-box">
+    <i class="bi bi-sun"></i> 
+    <div class="energy-text">
+      <p class="label">Energía generada</p>
+      <p class="value">{{ energy }} kWh</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({ energy: Number }); 
+</script>
+
+<style scoped>
+.energy-box {
+  background-color: var(--secondary-color);
+  padding: 20px;
+  border-radius: 8px;
+  text-align: center;
+}
+.label {
+  font-family: var(--font-general);
+}
+.value {
+  font-family: var(--font-featured);
+  font-size: 1.5rem;
+}
+</style>
---
+++ pages/energy.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import GeneratedEnergy from '~/components/energy/GeneratedEnergy.vue';
+
+</script>
+
+<template>
+    <GeneratedEnergy :energy="8.2"/>
+</template>
