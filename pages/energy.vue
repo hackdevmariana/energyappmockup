983a3c6e4cd818e3b4405e3a7077e85813a34f84
Fernando <fernando@juandemariana.org>
2025-06-08 07:20:44 +0200

    Fix energy view style.

--- pages/energy.vue
+++ pages/energy.vue
@@ -8,9 +8,12 @@ import EstimatedEarnings from '~/components/energy/EstimatedEarnings.vue';
 </script>
 
 <template>
+  <div class="container-90">
+
     <GeneratedEnergy :energy="8.2"/>
     <ConsumedEnergy :energy="6.1" />
     <ExportedEnergy :energy="3.7" />
     <StoredEnergy :energy="12.5" />
     <EstimatedEarnings :earnings="196.4" />
+  </div>
 </template>
